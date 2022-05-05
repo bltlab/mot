1495083b330696897d915788041a7c4620012544
Chester Palen-Michel <cpalenmichel@brandeis.edu>
2022-05-04 23:20:04 -0400

    Add params to wikipedia dumping script for min/max token size and min tokens per article

--- scripts/wikipediacorpus.py
+++ scripts/wikipediacorpus.py
@@ -7,14 +7,28 @@ https://github.com/panyang/Wikipedia_Word2vec/blob/master/v1/process_wiki.py
 """
 
 import sys
+from argparse import ArgumentParser
+
 from gensim.corpora import WikiCorpus
 
-def make_corpus(in_f, out_f):
+def make_corpus():
 
 	"""Convert Wikipedia xml dump file to text corpus"""
+	parser = ArgumentParser()
+	parser.add_argument("in_f")
+	parser.add_argument("out_f")
+	parser.add_argument("--min-tokens", default=1, type=int)
+	parser.add_argument("--token-min-len", default=1, type=int)
+	parser.add_argument("--token-max-len", default=25, type=int)
+	args = parser.parse_args()
 
-	output = open(out_f, 'w')
-	wiki = WikiCorpus(in_f)
+	output = open(args.out_f, 'w')
+	wiki = WikiCorpus(
+		args.in_f,
+		article_min_tokens=args.min_tokens,
+		token_min_len=args.token_min_len,
+		token_max_len=args.token_max_len
+	)
 
 	i = 0
 	for text in wiki.get_texts():
@@ -27,10 +41,4 @@ def make_corpus(in_f, out_f):
 
 
 if __name__ == '__main__':
-
-	if len(sys.argv) != 3:
-		print('Usage: python make_wiki_corpus.py <wikipedia_dump_file> <processed_text_file>')
-		sys.exit(1)
-	in_f = sys.argv[1]
-	out_f = sys.argv[2]
-	make_corpus(in_f, out_f)
+	make_corpus()
